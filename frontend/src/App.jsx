ee0cb855aa1c8a9d775b7f758c37256dfd93beb2
SRI01-create <sriazi.01@gmail.com>
2026-08-01 14:30:54 +0200

    Add ConfigTransfer component to main content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,6 +51,7 @@ export default function App() {
       <main className="flex-1 max-w-7xl w-full mx-auto px-6 py-8">
         {activeStep === 'config' && (
           <div>
+            <ConfigTransfer />
             <MaterialForm />
             <RCForm />
           </div>
